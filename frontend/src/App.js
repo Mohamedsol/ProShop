a73299f41f6caa6fb57b5a242b0b8aaf9dfabbdb
Mohamed <soltane.mohamed.achref@gmail.com>
2020-11-02 12:59:43 -0600

    Created HomeScreen Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1>Welcome to Our Shop</h1>
+    <Header/>
+    <Container>
+    <main className='py-3'>
+      <HomeScreen/>
+    </main>
+    </Container>
+    <Footer/>
+    
     </>
   );
 }
