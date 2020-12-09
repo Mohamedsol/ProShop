2ee36374015be13e432374c54d9b1a3474932ab8
Mohamed <soltane.mohamed.achref@gmail.com>
2020-12-09 08:13:54 -0600

    created JWT and custom auth middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 
@@ -11,11 +12,16 @@ dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)
